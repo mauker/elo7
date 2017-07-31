17f02b928c2cb1d5e98ac2fba1e851e92bacb8bc
Mauricio Olimpio <mauricio.olimpio@iterative.com.br>
2017-07-31 05:06:47 -0300

    Inicio do Desenvolvimento iOS

---
+++ ios/Elo7/Elo7/ViewController.swift
@@ -0,0 +1,33 @@
+//
+//  ViewController.swift
+//  Elo7
+//
+//  Created by Mauricio Olimpio on 31/07/17.
+//  Copyright © 2017 Mauricio Olimpio. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var webview: UIWebView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        
+        if let url = URL(string: "http://localhost:8080/carreiras.html"){
+            webview.loadRequest(URLRequest(url:url));
+            
+        }
+        
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
