f8867debba04bad3ff56febabb516d673199754e
Mauricio Olimpio <mauricio.olimpio@iterative.com.br>
2017-07-31 01:42:24 -0300

    HTML + Fonts

---
+++ server/server.js
@@ -0,0 +1,16 @@
+//importar a biblioteca do express
+var express = require("express");
+
+
+//criar a aplicação do express
+var app = express();
+
+
+//definimos o local da pasta publica
+app.use(express.static(__dirname + '/public'));  
+
+
+//vamos start o servidor
+var listener = app.listen(8080,function() {
+	console.log("Server Started at " + listener.address().port);
+})
